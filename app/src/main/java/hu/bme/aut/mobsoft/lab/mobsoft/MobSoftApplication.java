87a949cf2a64534b97ea9dba3803630d26616850
Szabo Gyorgy <szabo.gyorgy@nextent.hu>
2017-03-24 19:03:47 +0100

    Upgrade gradle version for instant run

--- app/src/main/java/hu/bme/aut/mobsoft/lab/mobsoft/MobSoftApplication.java
+++ app/src/main/java/hu/bme/aut/mobsoft/lab/mobsoft/MobSoftApplication.java
@@ -2,7 +2,6 @@ package hu.bme.aut.mobsoft.lab.mobsoft;
 
 import android.app.Application;
 
-import hu.bme.aut.mobsoft.lab.mobsoft.ui.DaggerMobSoftApplicationComponent;
 import hu.bme.aut.mobsoft.lab.mobsoft.ui.UIModule;
 
 /**
